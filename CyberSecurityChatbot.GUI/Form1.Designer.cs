bbef280097b6f5a7ecbf14f157e52fff245329b0
Acer <Acer@DESKTOP-DI6DA43>
2026-05-28 13:22:37 +0100

    Add project files.

---
+++ CyberSecurityChatbot.GUI/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace CyberSecurityChatbot.GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chatBox = new System.Windows.Forms.RichTextBox();
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // chatBox
+            // 
+            this.chatBox.BackColor = System.Drawing.Color.Black;
+            this.chatBox.Font = new System.Drawing.Font("Consolas", 11F);
+            this.chatBox.ForeColor = System.Drawing.Color.Lime;
+            this.chatBox.Location = new System.Drawing.Point(257, 69);
+            this.chatBox.Name = "chatBox";
+            this.chatBox.ReadOnly = true;
+            this.chatBox.Size = new System.Drawing.Size(425, 366);
+            this.chatBox.TabIndex = 0;
+            this.chatBox.Text = "";
+            // 
+            // InputBox
+            // 
+            this.InputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.InputBox.Location = new System.Drawing.Point(257, 441);
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(328, 24);
+            this.InputBox.TabIndex = 1;
+            // 
+            // sendButton
+            // 
+            this.sendButton.BackColor = System.Drawing.Color.LimeGreen;
+            this.sendButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.sendButton.ForeColor = System.Drawing.Color.Black;
+            this.sendButton.Location = new System.Drawing.Point(591, 441);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(91, 24);
+            this.sendButton.TabIndex = 2;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = false;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.titleLabel.ForeColor = System.Drawing.Color.LimeGreen;
+            this.titleLabel.Location = new System.Drawing.Point(252, 26);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(430, 29);
+            this.titleLabel.TabIndex = 3;
+            this.titleLabel.Text = "CYBERSECURITY AWARENESS BOT";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.InputBox);
+            this.Controls.Add(this.chatBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox chatBox;
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.Label titleLabel;
+    }
+}
+
---
+++ CyberSecurityChatbot.GUI/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace CyberSecurityChatbot.GUI
+{
+    public partial class Form1 : Form
+    {
+        ChatbotEngine bot = new ChatbotEngine();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chatBox.AppendText(" Welcome to the Cybersecurity Awareness Bot!\n");
+            chatBox.AppendText("Ask me about passwords, phishing, scams, or privacy.\n\n");
+
+            try
+            {
+                SoundPlayer player = new SoundPlayer("greetings.wav");
+                player.Play();
+            }
+            catch
+            {
+                chatBox.AppendText("Voice greeting could not be loaded.\n");
+            }
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            string userInput = InputBox.Text;
+
+            if (string.IsNullOrWhiteSpace(userInput))
+            {
+                MessageBox.Show("Please enter a message.");
+                return;
+            }
+
+            chatBox.AppendText("YOU: " + userInput + "\n");
+
+            string response = bot.GetResponse(userInput);
+
+            chatBox.AppendText("BOT: " + response + "\n\n");
+
+            InputBox.Clear();
+        }
+    }
+}
